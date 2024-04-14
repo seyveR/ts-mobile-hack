ade744811607abd0b32bc47924a0fcd0cd771d8d
mlxter <korznikov.m.a@edu.narfu.ru>
2024-04-14 07:49:30 +0300

    delete server data

--- djangoweb/Tickets/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-from .views import  AddRecord
-
-urlpatterns = [
-    path('', views.tickets, name = 'tickets'),
-    path('c_tickets/', views.c_tickets, name='c_tickets'),
-    path('add_record/', AddRecord.as_view()),
-]
